c4da1242b5552c0f7d768d0778306a2497d373aa
dip16igor <dip16igor@gmail.com>
2026-01-30 11:31:37 +0500

    exe

--- build_exe.bat
+++ build_exe.bat
@@ -1,11 +1,14 @@
 @echo off
 echo Building SDM4055A_Controller executable...
-pyinstaller --onefile ^
+python -m PyInstaller --onefile ^
     --windowed ^
     --name SDM4055A_Controller ^
     --hidden-import qt_material ^
+    --hidden-import config ^
+    --hidden-import config.config_loader ^
     --add-data "gui;gui" ^
     --add-data "hardware;hardware" ^
+    --add-data "config;config" ^
     main.py
 
 echo Build complete! Executable is in dist/SDM4055A_Controller.exe
